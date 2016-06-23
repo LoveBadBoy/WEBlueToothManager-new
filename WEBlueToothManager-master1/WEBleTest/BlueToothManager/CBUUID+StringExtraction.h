ea63616cc90ecdd2f2940ec54403548e0373e4f0
LoveBadBoy <1244026629@qq.com>
2016-06-23 11:49:03 +0800

    WEBlueToothManager-new
    蓝牙连接和获取服务和特征。与蓝牙外设之间进行数据的读取功能。

---
+++ WEBlueToothManager-master1/WEBleTest/BlueToothManager/CBUUID+StringExtraction.h
@@ -0,0 +1,15 @@
+//
+//  CBUUID+StringExtraction.h
+//  B100
+//
+//  Created by yuhanle on 15/8/19.
+//  Copyright (c) 2015年 yuhanle. All rights reserved.
+//
+
+#import <CoreBluetooth/CoreBluetooth.h>
+
+@interface CBUUID (StringExtraction)
+
+- (NSString *)representativeString;
+
+@end
